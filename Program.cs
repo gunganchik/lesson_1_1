17289c9708595041269d05972a946b8a132c441d
user <user@WIN-QM0DGDQ42DL>
2024-09-23 17:53:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Console.Write("Введите свое имя - ");
+string name = Console.ReadLine();
+Console.Write("Введите свою фамилию - ");
+string surname = Console.ReadLine();
+Console.Write("Введите свое отчество - ");
+string middle_name = Console.ReadLine();
+Console.WriteLine($"{surname} {name} {middle_name}");
